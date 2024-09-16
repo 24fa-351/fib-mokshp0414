0b1e1f055d98019f5a611137f2f1fd55d0d4c943
Moksh Patel <mokshp33@csu.fullerton.edu>
2024-09-15 17:16:20 -0700

    Assignment2

--- fib.c
+++ fib.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function to calculate Fibonacci using recursion
-int fibonacci_recursive(int n)
+// Iterative Fibonacci function
+unsigned long long int fibonacci_iterative(int n)
 {
-   if (n == 1)
-      return 0; // fib(1) = 0
-   if (n == 2)
-      return 1; // fib(2) = 1
-   return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
-}
-
-// Function to calculate Fibonacci using iteration
-int fibonacci_iterative(int n)
-{
-   if (n == 1)
-      return 0; // fib(1) = 0
-   if (n == 2)
-      return 1; // fib(2) = 1
-   int a = 0, b = 1, c;
-   for (int i = 3; i <= n; i++) // Start from 3 since fib(1) and fib(2) are known
+   if (n > 93)
    {
-      c = a + b;
+      printf("Overflow: The largest Fibonacci number that can be accurately represented is F(93).\n");
+      exit(1); // Exit if the number exceeds the limit
+   }
+
+   if (n == 1)
+      return 0; // Special case: F(1) = 0
+   if (n == 2)
+      return 1; // Special case: F(2) = 1
+
+   unsigned long long int a = 0, b = 1, temp;
+   for (int i = 3; i <= n; i++)
+   {
+      temp = a + b;
       a = b;
-      b = c;
+      b = temp;
    }
    return b;
+}
+
+// Recursive Fibonacci function
+unsigned long long int fibonacci_recursive(int n)
+{
+   if (n > 50)
+   {
+      printf("Recursive method: Taking too long for large inputs.\n");
+      exit(1); // Exit if the input is too large for recursion
+   }
+
+   if (n == 1)
+      return 0; // Special case: F(1) = 0
+   if (n == 2)
+      return 1; // Special case: F(2) = 1
+
+   return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
 }
 
 int main(int argc, char *argv[])
@@ -36,40 +50,40 @@ int main(int argc, char *argv[])
       return 1;
    }
 
-   // First argument: an integer
-   int first_arg = atoi(argv[1]);
+   int input_num = atoi(argv[1]);
+   char method = argv[2][0];
+   char *filename = argv[3];
 
-   // Open the file and read the integer from it
-   FILE *file = fopen(argv[3], "r");
+   FILE *file = fopen(filename, "r");
    if (file == NULL)
    {
-      printf("Error opening file %s\n", argv[3]);
+      printf("Error: Could not open file %s\n", filename);
       return 1;
    }
-   int file_number;
-   fscanf(file, "%d", &file_number);
+
+   int file_num;
+   fscanf(file, "%d", &file_num);
    fclose(file);
 
-   // Sum the two numbers
-   int N = first_arg + file_number;
+   int N = input_num + file_num;
 
-   // Compute the Nth Fibonacci number based on the second argument (r or i)
-   int result;
-   if (argv[2][0] == 'r')
-   {
-      result = fibonacci_recursive(N);
-   }
-   else if (argv[2][0] == 'i')
+   unsigned long long int result;
+
+   if (method == 'i')
    {
       result = fibonacci_iterative(N);
    }
+   else if (method == 'r')
+   {
+      result = fibonacci_recursive(N);
+   }
    else
    {
-      printf("Invalid option %s. Use 'r' for recursive or 'i' for iterative.\n", argv[2]);
+      printf("Invalid method. Use 'r' for recursive or 'i' for iterative.\n");
       return 1;
    }
 
-   // Output the result
-   printf("%d\n", result);
+   printf("%llu\n", result);
+
    return 0;
 }
